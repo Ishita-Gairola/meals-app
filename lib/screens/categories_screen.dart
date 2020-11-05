957443d4dd00e1c8d1fc9dfa8c8e21f07982b4a5
Ishita-Gairola <ishitagairola777@gmail.com>
2020-11-05 23:08:34 +0530

    First commit.

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,34 @@
+//this is for controlling all content on the screen
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio:
+              3 / 2, //how item will be sized with height and width ratio
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
